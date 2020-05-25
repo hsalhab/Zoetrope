04bb50a3e4e95736bcb4fc7bb3e6848e6fae36c0
hsalhab <husam_salhab@brown.edu>
2020-05-25 12:00:13 -0400

    added bootstrap container

--- manager/frontend/src/components/App.js
+++ manager/frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Container, Image, Button } from 'react-bootstrap';
+import { Container, Image, Button, Row, Col } from 'react-bootstrap';
 
 import '../styles/main.scss';
 
@@ -8,8 +8,19 @@ class App extends Component {
   render() {
     return (
       <Container>
-        <Image></Image>
-        <Button>Click Here</Button>
+        <br/><br/><br/>
+        {/* TODO: center vertically */}
+        <Row>
+          <Col className='text-center'>
+            <Image src=""></Image>
+          </Col>
+        </Row>
+        <br/>
+        <Row>
+          <Col className='text-center'>
+            <Button>Click Here</Button>
+          </Col>
+        </Row>
       </Container>
     )
   }
