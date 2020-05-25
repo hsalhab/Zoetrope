2feb0d49fa983469b96c8928c116e7952410c2db
hsalhab <husam_salhab@brown.edu>
2020-05-25 10:11:53 -0400

    removed sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const devMode = process.env.NODE_ENV !== 'production';
+
 module.exports = {
   module: {
     rules: [
@@ -7,30 +9,7 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
-      },
-      {
-        test: /\.(scss|sass|css)$/,
-        exclude: /node_modules/,
-        loaders: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: true,
-              importLoaders: 1,
-              localIdentName: '[local]___[hash:base64:5]'
-            }
-          },
-        'sass-loader',
-        ]
-      },
+      }
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-    })
-  ],
+  }
 }
