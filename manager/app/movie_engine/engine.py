f21390252a1bb3759bdf93e8fbbd50eaca70fc16
hsalhab <husam_salhab@brown.edu>
2020-05-26 16:28:21 -0400

    removed runtime scraping

--- manager/app/movie_engine/engine.py
+++ manager/app/movie_engine/engine.py
@@ -4,7 +4,6 @@ from ..models import Movie
 
 def get_random_movie():
     movie = Movie.objects.random()
-    poster = get_poster_link(movie.imdbid)
     data = {
         'title': movie.title,
         'poster': movie.poster
