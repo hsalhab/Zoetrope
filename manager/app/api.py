9bf5c01cf22163c5e440f7c4b5c81fa2819e83a5
hsalhab <husam_salhab@brown.edu>
2020-05-26 03:44:12 -0400

    added random movie engine

--- manager/app/api.py
+++ manager/app/api.py
@@ -2,6 +2,7 @@ from app.models import App
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from .serializers import AppSerializer
+from .movie_engine import engine
 
 class AppViewSet(viewsets.ModelViewSet):
     queryset = App.objects.all()
@@ -12,7 +13,5 @@ class AppViewSet(viewsets.ModelViewSet):
 
 class MovieViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
-        data = {
-            'message': 'hi how are ya'
-        }
+        data = engine.get_random_movie()
         return Response(data)
--- manager/app/models.py
+++ manager/app/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from django.db.models.aggregates import Count
+from random import randint
 
 class App(models.Model):
     message = models.CharField(max_length=500, blank=True)
+
+class MovieManager(models.Manager):
+    def random(self):
+        count = self.aggregate(count=Count('movieid'))['count']
+        random_index = randint(0, count - 1)
+        return self.all()[random_index]
+
+class Movie(models.Model):
+    objects = MovieManager()
+    movieid = models.BigIntegerField(db_column='movieId', primary_key=True)
+    title = models.TextField(blank=True, null=True)
+    genres = models.TextField(blank=True, null=True)
+    imdbid = models.TextField(db_column='imdbId', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'movies'
